0803941c42178e4887bd70b889332264308e3fef
Emre Gokyar <emregokyar1940@gmail.com>
2025-04-26 03:23:30 +0300

    Adding a new User
    - Creation method for creating new user and supporting elements

--- src/main/java/com/socialmedia/service/UserService.java
+++ src/main/java/com/socialmedia/service/UserService.java
@@ -1,16 +1,47 @@
 package com.socialmedia.service;
 
+import com.socialmedia.entity.Regular;
+import com.socialmedia.entity.User;
+import com.socialmedia.repository.RegularRepository;
 import com.socialmedia.repository.UserRepository;
+import com.socialmedia.repository.UserTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final UserTypeRepository userTypeRepository;
+    private final RegularRepository regularRepository;
+    //private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserTypeRepository userTypeRepository, RegularRepository regularRepository) {
         this.userRepository = userRepository;
+        this.userTypeRepository = userTypeRepository;
+        this.regularRepository = regularRepository;
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User addNewUser(User user) {
+        //user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setIsActive(true);
+        user.setRegistrationDate(new Date(System.currentTimeMillis()));
+        user.setUserType(userTypeRepository.getReferenceById(1));
+        User savedUser = userRepository.save(user);
+        Regular regular = new Regular();
+        regular.setUser(user);
+        regularRepository.save(regular);
+        return savedUser;
     }
 }
-    
